f5da98d2fadf59fd9ec1c4a994ec49c7f147c83e
Ryan Kent <hello@interphased.com>
2016-06-22 12:48:19 -0400

    update dependencies, fix gulp-notify, and watch subdirectories

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Require
 var gulp = require('gulp'),  
     less = require('gulp-less'),
@@ -6,8 +8,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
-    growl = require('gulp-notify-growl');
+    notify = require('gulp-notify');
 
 
 // Paths
@@ -26,31 +27,35 @@ var paths = {
 
 // CSS
 gulp.task('css', function() {  
-  return gulp.src(paths.dev.less+'application.less')
-    .pipe(less())
-    .pipe(gulp.dest(paths.production.css))
-    .pipe(minify({keepSpecialComments:0}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.production.css));
+    return gulp.src(paths.dev.less+'application.less')
+        .pipe(less())
+        .pipe(gulp.dest(paths.production.css))
+        .pipe(minify({keepSpecialComments:0}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.production.css))
+        .pipe(notify("CSS compilation successful!"));
 });
 
 
 // JS
 gulp.task('js', function(){  
-  return gulp.src([
-      // paths.dev.vendor+'jquery/dist/jquery.js',
-      paths.dev.js+'application.js'
+    return gulp.src([
+        // paths.dev.vendor+'jquery/dist/jquery.js',
+        paths.dev.js+'application.js'
     ])
     .pipe(concat('application.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.production.js));
+    .pipe(gulp.dest(paths.production.js))
+    .pipe(notify("JS compilation successful!"));
 });
 
 
 // Tasks
 gulp.task('watch', function() {  
-  gulp.watch(paths.dev.less + '/*.less', ['css']);
-  gulp.watch(paths.dev.js + '/*.js', ['js']);
+    gulp.watch(paths.dev.less + '/*.less', ['css']);
+    gulp.watch(paths.dev.less + '/*/*.less', ['css']);
+    gulp.watch(paths.dev.js + '/*.js', ['js']);
+    gulp.watch(paths.dev.js + '/*/*.js', ['js']);
 });
 
 gulp.task('default', ['css', 'js']);  
